db679fcc12213d0d7ed72842af317a96ec5eced5
manimrc <maninder011@gmail.com>
2024-11-08 13:21:22 +0530

    implemented the env file for secrets

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,3 @@
-// src/components/HomePage.js
-
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -12,12 +10,12 @@ function HomePage() {
     async function fetchAndLogUserInfo() {
       try {
         // Fetch user info from IPinfo
-        const response = await axios.get(`https://ipinfo.io?token=df2eeeb69b4fd7`);
+        const response = await axios.get(`https://ipinfo.io?token=${process.env.REACT_APP_IPINFO_TOKEN}`);
         const userInfo = response.data;
 
         // Log data to Logflare
         await axios.post('https://api.logflare.app/logs', {
-          log_key: '38b11f8f-9c8d-4b0a-b71b-c94d40d2a6ba',  // Replace with your actual Logflare source key
+          log_key: process.env.REACT_APP_LOGFLARE_SOURCE_ID,
           message: "User Visit",
           metadata: {
             ip: userInfo.ip,
@@ -30,7 +28,7 @@ function HomePage() {
           }
         }, {
           headers: {
-            'X-API-KEY': 'fabe2d6c6a0cfe3fd28c59c810c7e3b72262a628ef10912c2def3d4e527b55aa',  // Replace with your Logflare API key
+            'X-API-KEY': process.env.REACT_APP_LOGFLARE_API_KEY,
           }
         });
 
